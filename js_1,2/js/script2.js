8f97245a10eef9e1da6575044b343715384abaea
Maxim Vaschenko <maxvaschenko@gmail.com>
2017-06-17 13:06:44 +0300

    исправил 2 часть 2 задания через цикл for

--- js_1,2/js/script2.js
+++ js_1,2/js/script2.js
@@ -1,26 +1,26 @@
+/**
+ * Created by User on 28.05.2017.
+ */
 var arr = [];
 
 for (var i = 0; i < 5; i++) {
-
-    arr.push(prompt("Введите имя"));
-
+    arr.push(prompt('Введите имя', name));
 }
 
-userName = prompt("Введите имя пользаователя");
+console.log(arr);
 
-if (userName == arr[0]) {
-    alert(userName + ", вы успешно вошли");
-} else if (userName == arr[1]) {
-    alert(userName + ", вы успешно вошли");
-} else if (userName == arr[2]) {
-    alert(userName + ", вы успешно вошли");
-} else if (userName == arr[3]) {
-    alert(userName + ", вы успешно вошли");
-} else if (userName == arr[4]) {
-    alert(userName + ", вы успешно вошли");
-} else {
-    alert("Ошибка!")
+userName = prompt('Введите имя пользователя');
+
+for (var i = 0; i < 5; i++) {
+    if (arr[i] === userName) {
+        alert(userName + ', вы успешно вошли');
+        break
+    } else {
+        alert('Error');
+        break
+    }
 }
+
 
 console.log(arr);
 console.log(userName);
