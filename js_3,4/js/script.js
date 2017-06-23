3ee8fea730b943888e790143e313c4a317103719
Maxim Vaschenko <maxvaschenko@gmail.com>
2017-06-24 00:08:35 +0300

    добавил цикл для вопросов и ответов

--- js_3,4/js/script.js
+++ js_3,4/js/script.js
@@ -12,9 +12,24 @@ testName.innerHTML = 'Тест по программированию';
 testContainer.appendChild(testName);
 
 var questions = document.createElement('ul');
+questions.style.listStyle = "none";
+questions.style.paddingLeft = 0;
 for (var i = 0; i < 3; i++) {
-    var answers = document.createElement("li");
-    questions.appendChild(answers);
+    var questionsItems = document.createElement("li");
+    questionsItems.innerHTML = (i + 1) + ". Вариант ответа №" + (i + 1);
+    questions.appendChild(questionsItems);
+    for (var j = 0; j < 3; j++) {
+        var answers = document.createElement('ul');
+        var answersItems = document.createElement("li");
+        var answerItemLabel = document.createElement("input");
+        answerItemLabel.type = "checkbox";
+        answersItems.innerHTML = "Вариант ответа №" + (j + 1);
+        answers.style.display = "flex";
+        answers.style.listStyle = "none";
+        answers.insertBefore(answerItemLabel, answersItems[0]);
+        questionsItems.appendChild(answers);
+        answers.appendChild(answersItems);
+    }
 }
 testContainer.appendChild(questions);
 
