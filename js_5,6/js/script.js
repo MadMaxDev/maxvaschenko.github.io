93619c7ce4d74785b304c6ccfbc50b1c1f3c0b68
Maxim Vaschenko <maxvaschenko@gmail.com>
2017-07-09 13:54:19 +0300

    табы и всплывающие подсказки

--- js_5,6/js/script.js
+++ js_5,6/js/script.js
@@ -1,21 +1,3 @@
-// /**
-//  * Created by User on 28.06.2017.
-//  */
-// function start_time() {
-//
-//     clearInterval(start_time_interval);
-//     start_time_interval = setInterval(function(){
-//         var new_date = new Date() - this_date;
-//         var sec   = Math.abs(Math.floor(new_date/1000)%60); //sek
-//         var min   = Math.abs(Math.floor(new_date/1000/60)%60); //min
-//         var hours = Math.abs(Math.floor(new_date/1000/60/60)%24); //hours
-//         if (sec.toString().length   == 1) sec   = '0' + sec;
-//         if (min.toString().length   == 1) min   = '0' + min;
-//         if (hours.toString().length == 1) hours = '0' + hours;
-//         $('.timer').text(hours + ':' + min + ':' + sec);
-//     },100);
-//
-// };
 var a = 0;
 var b = 0;
 var c = 0;
@@ -39,7 +21,9 @@ function start_time() {
 
     var this_date = new Date();
 
+
     startTimeInterval = setInterval(function () {
+
         var new_date = new Date() - this_date;
         var msec = Math.abs(Math.floor(new_date) % 1000); //msek
         var sec = Math.abs(Math.floor(new_date / 1000) % 60); //sek
@@ -55,6 +39,8 @@ function start_time() {
     }, 10);
 }
 
-// function stopTime() {
-//     clearInterval(startTimeInterval);
-// }
+function stopTime() {
+    clearInterval(startTimeInterval);
+}
+
+
