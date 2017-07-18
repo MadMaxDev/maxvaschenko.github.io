22faa95ac02dd51351c3f5778b0e982b42e067af
vaschenkom <maxvaschenko@gmail.com>
2017-07-18 23:36:16 +0300

    выпадающее меню через клик

--- js_9,10/js/script.js
+++ js_9,10/js/script.js
@@ -3,7 +3,7 @@
  */
 $('.slider').slick({
     nextArrow: $('#next'),
-    prevArrow: $('#prev'),
+    prevArrow: $('#prev')
 });
 
 $(function () {
@@ -20,32 +20,44 @@ $('.nonCss').on('click', function () {
 
 var subMenu1 = $('.submenu_1');
 var subMenu2 = $('.submenu_2');
-var subMenu3 = $('.submenu_3');
+// var subMenu3 = $('.submenu_3');
 
-subMenu1.mouseover(function () {
-    subMenu2.animate({
-        backgroundColor: "#a72222"
-    }, 600, function () {
-        subMenu2.addClass("active");
-        $('#submenu_1_a').css('margin-left', 20 + 'px');
-        subMenu1.css('padding-left', 0);
-        subMenu1.css('padding-right', 0);
-    });
+subMenu1.on('click', function () {
+    subMenu2.slideToggle('slow');
 });
-$('#submenu_2_main_item').hover(
-    function () {
-        subMenu3.addClass("active");
-        subMenu3.css({
-            'margin-left': 70 + 'px',
-            'position': 'absolute',
-            'margin-top': -60 + 'px',
-            'background-color': '#e2d647'
-        });
-    },
-    function () {
-        subMenu3.removeClass("active");
-    }
-);
 
 
-console.log(subMenu1);
+// subMenu1.hover(
+//     function () {
+//     subMenu2.animate({
+//         backgroundColor: "#a72222"
+//     }, 'slow', function () {
+//         subMenu2.addClass("active");
+//         $('#submenu_1_a').css('margin-left', 20 + 'px');
+//         subMenu1.css('padding-left', 0);
+//         subMenu1.css('padding-right', 0);
+//     });
+//     },
+//     function () {
+//         subMenu2.animate({},'slow', function () {
+//             subMenu2.removeClass("active");
+//         });
+//     }
+//     );
+// $('#submenu_2_main_item').hover(
+//     function () {
+//         subMenu3.addClass("active");
+//         subMenu3.css({
+//             'margin-left': 70 + 'px',
+//             'position': 'absolute',
+//             'margin-top': -60 + 'px',
+//             'background-color': '#e2d647'
+//         });
+//     },
+//     function () {
+//         subMenu3.removeClass("active");
+//     }
+// );
+//
+//
+// console.log(subMenu1);
