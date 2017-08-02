20aa9dd9a4e90dca40683bf75e8ea79e1a00c911
maxvaschenko <maxvaschenko@gmail.com>
2017-08-03 00:01:56 +0300

    тест на hbs

---
+++ js_13,14/js/script.js
@@ -0,0 +1,26 @@
+var source   = $('#testPage').html();
+var template = Handlebars.compile(source);
+
+var data = {
+    testName: 'Тест по математике',
+    questions: [
+        {
+            title: '1. Сколько будет 2^4?',
+            answers: ['4', '8', '32'],
+            correctAnswers: [3]
+        },
+        {
+            title: '2. Какая фигура имеет равные стороны?',
+            answers: ['Квадрат', 'Равнобедренный треугольник', 'Прямоугольник'],
+            correctAnswers: [1]
+        },
+        {
+            title: '3. Чему равно (2+2)*2?',
+            answers: ['4', '6', '8'],
+            correctAnswers: [3]
+        }
+    ]
+};
+
+var html = template(data);
+$(document.body).append(html);
