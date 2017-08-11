57383dfa47a62a3eab2b5ff70a6ed704d80a7092
maxvaschenko <maxvaschenko@gmail.com>
2017-08-11 15:03:35 +0300

    тест с модалькой

--- js_13,14/js/script.js
+++ js_13,14/js/script.js
@@ -11,20 +11,20 @@ var dataSource = {
     questions: [
         {
             title: '1. Сколько будет 2^4?',
-            answers: ['4', '8', '32'],
-            correctAnswers: [3]
+            answers: ['4', '8', '16']
         },
         {
-            title: '2. Какая фигура имеет равные стороны?',
-            answers: ['Квадрат', 'Равнобедренный треугольник', 'Прямоугольник'],
-            correctAnswers: [1]
+            title: '2. Какая фигура имеет все равные стороны?',
+            answers: ['Квадрат', 'Равнобедренный треугольник', 'Прямоугольник']
         },
         {
             title: '3. Чему равно (2+2)*2?',
-            answers: ['4', '6', '8'],
-            correctAnswers: [3]
+            answers: ['4', '6', '8']
+
         }
-    ]
+
+    ],
+    correctAnswers : '[0, 0, 1, 1, 0, 0, 0, 0, 1]'
 };
 
 var html = template(data);
@@ -34,5 +34,34 @@ $(document.body).append(html);
 var testData = JSON.stringify(dataSource);
 localStorage.setItem("testDataKey", testData);
 
+// Проверка
 
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+const btn = document.getElementById("btn");
 
+const getInptState = inputs => inputs.map(input => input.checked);
+
+btn.addEventListener("click", () => {
+    const state = getInptState([...checkboxes]);
+    var resultStr = (`${JSON.stringify(state)}`);
+    var result = JSON.parse(resultStr);
+
+    var correctResultsVal = JSON.parse(data.correctAnswers);
+    var correctResults = [];
+    for(var i=0; i<correctResultsVal.length; i++) {
+        if (correctResultsVal[i]==0) {
+            correctResults.push(false);
+        } else correctResults.push(true);
+    }
+    var resultOfTest = _.isEqual(result, correctResults);
+    if (resultOfTest==true) {
+        $("#testResult1").css("display", "block");
+        $("#testResult2").css("display", "none");
+    } else {
+        $("#testResult1").css("display", "none");
+        $("#testResult2").css("display", "block");
+    }
+$("#myModalBox").modal('show');
+$('input').prop('checked', false);
+
+});
