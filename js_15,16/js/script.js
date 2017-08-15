312ea19d5f7cfca663a94444af6f34ff252d2f53
maxvaschenko <maxvaschenko@gmail.com>
2017-08-15 20:18:05 +0300

    макет по примеру без шаблонов

---
+++ js_15,16/js/script.js
@@ -0,0 +1,41 @@
+function newRender(){
+    'use strict';
+    debugger;
+    var container = document.getElementById('root'),
+        source = document.getElementById('article').textContent,
+        compiled = _.template(source),
+        apiUrl =
+            'https://pixabay.com/api/?key=6176850-04154e69eb28ef97e770c0325&q=cars+girls&per_page=9';
+
+    var promise = fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            return response.json();
+        }
+        throw new Error ('ERROR while fetching!');
+    }).then(function(data) {
+        var articles = _.map(data.articles, function(item){
+            return {
+                title: item.id,
+                img: item.previewURL,
+                onPage:item.pageURL
+            };
+        });
+
+        console.log(articles);
+
+        render(compiled, articles,container);
+    }).catch(function(error) {
+        console.log (error);
+    });
+
+    function render(template, data, parent){
+        var htmlString ="";
+        _.forEach(data, function(item){
+            htmlString+=template(item);
+        });
+        parent.innerHTML+=htmlString;
+    }
+
+};
+
+newRender();
