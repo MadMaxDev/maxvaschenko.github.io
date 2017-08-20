812986c2ffc52d6ca008b10dc0c420aaa9f3765a
maxvaschenko <maxvaschenko@gmail.com>
2017-08-20 18:05:25 +0300

    добавил input и кнопки поиск и очистить

--- js_15,16/js/script.js
+++ js_15,16/js/script.js
@@ -1,43 +1,54 @@
 function newRender(){
     'use strict';
-    var container = document.getElementById('root'),
-        source = document.getElementById('article').textContent,
-        compiled = _.template(source),
-        apiUrl =
-            'https://pixabay.com/api/?key=6176850-04154e69eb28ef97e770c0325&q=cars+girls&per_page=9';
+    function search() {
+        var searchQuery = document.getElementById("searchInput").value;
 
-    var promise = fetch(apiUrl)
-        .then(function(response) {
-            if (response.ok) {
-                return response.json();
-            }
-            throw new Error ('ERROR while fetching!');
-        }).then(function(data) {
-        var articles = _.map(data.hits, function(item){
-            return {
-                user: item.user,
-                img: item.previewURL,
-                onPage:item.pageURL
-            };
+        var container = document.getElementById('root'),
+            source = document.getElementById('article').textContent,
+            compiled = _.template(source),
+            apiUrl =
+                'https://pixabay.com/api/?key=6176850-04154e69eb28ef97e770c0325&q='+searchQuery+'&per_page=9';
+
+        var promise = fetch(apiUrl)
+            .then(function(response) {
+                if (response.ok) {
+                    return response.json();
+                }
+                throw new Error ('ERROR while fetching!');
+            }).then(function(data) {
+            var articles = _.map(data.hits, function(item){
+                return {
+                    user: item.user,
+                    img: item.previewURL,
+                    onPage:item.pageURL
+                };
+            });
+            render(compiled, articles, container);
+        }).catch(function(error) {
+            console.log (error);
         });
-        console.log('data', data);
-        console.log('articles', articles);
-        console.log ('render', render);
 
-        render(compiled, articles, container);
-    }).catch(function(error) {
-        console.log (error);
+        function render(template, data, parent){
+            var htmlString ='';
+            _.forEach(data, function(item){
+                htmlString+=template(item);
+            });
+            parent.innerHTML+=htmlString;
+        }
+        var clearButton = document.getElementById('clear');
+        clearButton.addEventListener("click", function () {
+            searchQuery = "";
+            location.reload(true);
+        });
+    }
+    var searchBtn = document.getElementById("btn");
+    searchBtn.addEventListener("click", function () {
+        search()
     });
 
-    function render(template, data, parent){
-        var htmlString ='';
-        _.forEach(data, function(item){
-            htmlString+=template(item);
-        });
-        parent.innerHTML+=htmlString;
-        console.log('htmlString', htmlString);
-    }
-
 };
-
 newRender();
+
+
+
+
