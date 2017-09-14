abfa99dea7479269fe13dbe42da4ae83c73cda02
mvashchenko <mvashchenko@itteam.io>
2017-09-14 15:36:57 +0300

    my triaining files

---
+++ js-react/src/components/App.jsx
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import FlavorForm from '../components/sayHello';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+        <FlavorForm/>
+    )
+  }
+}
+
+
+export default App;
+
+
