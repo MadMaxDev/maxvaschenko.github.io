8a35b6f16e676c5c8d178be5f8ec64ba386ce625
mvashchenko <mvashchenko@itteam.io>
2017-09-22 11:19:39 +0300

    сборка

--- training/src/index.jsx
+++ training/src/index.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 
-// import 'vanilla';
-
 import App from '@/containers/App';
 import store from '@/store';
 
@@ -14,41 +12,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-
-
-// const onAdd = () => {
-//   store.dispatch({
-//     type: 'ADD',
-//     payload: 10
-//   });
-// };
-//
-// const onSub = () => {
-//   store.dispatch({
-//     type: 'SUB',
-//     payload: 10
-//   });
-// };
-//
-// const Counter = ({value, onAdd, onSub}) => (
-//   <div className="container card-panel center-align">
-//     <h1>{value}</h1>
-//     <button className="btn" onClick={onAdd}>+</button>
-//     <button className="btn" onClick={onSub}>-</button>
-//   </div>
-// );
-//
-//
-// const render = () => {
-//   ReactDOM.render(
-//     <Counter
-//       value={store.getState()}
-//       onAdd={onAdd}
-//       onSub={onSub}
-//     />,
-//     document.getElementById('root')
-//   );
-// };
-//
-// store.subscribe(render);
-// render();
