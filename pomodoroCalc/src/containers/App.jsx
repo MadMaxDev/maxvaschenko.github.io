6ff16b85824a40a76f2de6709f4359a4e0acff69
mvashchenko <mvashchenko@itteam.io>
2017-09-26 18:12:55 +0300

    poomodoro timer sec + min

--- pomodoroCalc/src/containers/App.jsx
+++ pomodoroCalc/src/containers/App.jsx
@@ -4,25 +4,32 @@ import ReactTimeout from 'react-timeout';
 import __StyledElem__ from '../containers/styled';
 import __Button__ from '../components/button/index';
 
+
+
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       timerStarted: false,
-      seconds: 59,
-      targetMin: 25,
+      seconds: 0,
+      targetMin: 5,
+      targetMinFroze: 0,
       restMin: 5
     };
     this.handleTargetPlusClick = this.handleTargetPlusClick.bind(this);
     this.handleTargetMinusClick = this.handleTargetMinusClick.bind(this);
     this.handleRestPlusClick = this.handleRestPlusClick.bind(this);
     this.handleRestMinusClick = this.handleRestMinusClick.bind(this);
+    this.timerHandler = this.timerHandler.bind(this);
     this.timerOn = this.timerOn.bind(this);
+    this.timerOff = this.timerOff.bind(this);
+
   }
 
   handleTargetPlusClick(e) {
     this.setState({
-      targetMin: (this.state.targetMin > 0) ? this.state.targetMin + 1 : 0
+      targetMin: (this.state.targetMin >= 0) ? this.state.targetMin + 1 : 0
     });
   }
 
@@ -43,18 +50,29 @@ class App extends Component {
     })
   }
 
-  timerOn(e) {
+  timerOn() {
     this.setState({timerStarted: !this.state.timerStarted});
-    setInterval(() => {this.setState(
-      {seconds: (this.state.seconds === 0) ? 59 : this.state.seconds - 1,
-       targetMin: (this.state.seconds === 0) ? this.state.targetMin - 1 : this.state.targetMin,
-      }
-    )
+    this.setState({targetMinFroze: this.state.targetMin});
+    // this.setState({targetMin: this.state.targetMin - 1});
+    this.stopTimer = setInterval(() =>
+    {this.setState(
+      {seconds: (this.state.seconds === 0) ? (this.state.targetMin===0 ? 0 : 59): this.state.seconds - 1,
+       targetMin: (this.state.targetMin > 0) ? ((this.state.seconds === 0) ? this.state.targetMin - 1 : this.state.targetMin) : 0}
+    );
     }, 1000);
   }
 
+  timerOff() {
+    this.setState({timerStarted: !this.state.timerStarted});
+    clearInterval(this.stopTimer)
+  }
+
+  timerHandler(e) {
+    {this.state.timerStarted ? this.timerOff() : this.timerOn()}
+  }
+
+
   render() {
-    const secondsInTimer = 60 - this.state.seconds;
     return (
       <__StyledElem__>
         <div className="container-timer">
@@ -67,14 +85,14 @@ class App extends Component {
             </div>
             <div className="targetMin">
               <p>Target</p>
-              <__Button__ buttonName="+" clickEvent = {this.handleTargetPlusClick}></__Button__>
-              {this.state.targetMin}
-              <__Button__ buttonName="-" clickEvent = {this.handleTargetMinusClick}></__Button__>
+              <__Button__ buttonName="+" clickEvent = {this.handleTargetPlusClick} buttonEnable={this.state.timerStarted}></__Button__>
+              {this.state.timerStarted ? this.state.targetMinFroze : this.state.targetMin}
+              <__Button__ buttonName="-" clickEvent = {this.handleTargetMinusClick} buttonEnable={this.state.timerStarted}></__Button__>
             </div>
           </div>
-          <div className="timerLog" onClick={this.timerOn}>
+          <div className="timerLog" onClick={this.timerHandler}>
             <span>
-              {this.state.targetMin}:{this.state.timerStarted ? this.state.seconds : '00'}
+              {this.state.targetMin}:{this.state.seconds < 10 ? '0'+this.state.seconds : this.state.seconds}
             </span>
           </div>
         </div>
